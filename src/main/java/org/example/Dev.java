1a5794fe837748822de5eb52fbc94c738fa95571
LAPTOP-P2B4QFJN\Sumedha Sure <suresumedha@gmail.com>
2025-11-03 13:59:07 -0600

    created dev class,constructor and getter,setter methods,build method

---
+++ src/main/java/org/example/Dev.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+
+public class Dev {
+    private Computer com;
+
+
+
+    public Dev(){
+        System.out.println("Dev Constructor");
+    }
+
+
+
+
+
+    public Computer getCom() {
+        return com;
+    }
+
+    public void setCom(Computer com) {
+        this.com = com;
+    }
+
+    public void build(){
+
+        System.out.println("Working on Awesome project");
+         com.compile();
+    }
+}
